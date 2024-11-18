c515edd31cb85e239153f6f0b16e856c734b9a03
hlarsen <harley@harleylarsen.com>
2024-11-17 19:13:33 -0800

    format code and rename for clarity

--- dltool.py
+++ dltool.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+#
+# This script will take "fix DATs" (DATs made up of missing ROMs) and attempt to download them from Myrient.
+#
+
 import os
 import re
 import math
@@ -11,27 +16,24 @@ import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 from progressbar import ProgressBar, Bar, ETA, FileTransferSpeed, Percentage, DataSize
 
-#Define constants
-#Myrient HTTP-server addresses
-MYRIENTHTTPADDR = 'https://myrient.erista.me/files/'
-#Catalog URLs, to parse out the catalog in use from DAT
-CATALOGURLS = {
+# Constants
+CATALOG_URLS = {
     'https://www.no-intro.org': 'No-Intro',
-    'http://redump.org/': 'Redump'
+    'https://redump.org/': 'Redump'
 }
-#Postfixes in DATs to strip away
-DATPOSTFIXES = [
-    ' (Retool)'
+CHUNK_SIZE = 8192
+DAT_NAME_FIXES = [
+    'fixDat_'
+    ' (Retool)',
 ]
-#Chunk sizes to download
-CHUNKSIZE = 8192
-#Headers to use in HTTP-requests
-REQHEADERS = {
+MYRIENT_URL = 'https://myrient.erista.me/files/'
+REQ_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'
 }
 
-#Print output function
+
+# Print output function
 def logger(str, color=None, rewrite=False):
     colors = {'red': '\033[91m', 'green': '\033[92m', 'yellow': '\033[93m', 'cyan': '\033[96m'}
     if rewrite:
@@ -41,7 +43,8 @@ def logger(str, color=None, rewrite=False):
     else:
         print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} | {str}')
 
-#Input request function
+
+# Input request function
 def inputter(str, color=None):
     colors = {'red': '\033[91m', 'green': '\033[92m', 'yellow': '\033[93m', 'cyan': '\033[96m'}
     if color:
@@ -50,9 +53,10 @@ def inputter(str, color=None):
         val = input(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} | {str}')
     return val
 
-#Scale file size
+
+# Scale file size
 def scale1024(val):
-    prefixes=['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+    prefixes = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
     if val <= 0:
         power = 0
     else:
@@ -61,13 +65,16 @@ def scale1024(val):
     unit = prefixes[power]
     return scaled, unit
 
-#Exit handler function
+
+# Exit handler function
 def exithandler(signum, frame):
     logger('Exiting script!', 'red')
     exit()
+
+
 signal.signal(signal.SIGINT, exithandler)
 
-#Generate argument parser
+# Generate argument parser
 parser = argparse.ArgumentParser(
     add_help=False,
     formatter_class=argparse.RawTextHelpFormatter,
@@ -79,29 +86,34 @@ parser = argparse.ArgumentParser(
         the matching files from Myrient.\033[00m
     '''))
 
-#Add required arguments
+# Add required arguments
 requiredargs = parser.add_argument_group('\033[91mRequired arguments\033[00m')
-requiredargs.add_argument('-i', dest='inp', metavar='nointro.dat', help='Input DAT-file containing wanted ROMs', required=True)
-requiredargs.add_argument('-o', dest='out', metavar='/data/roms', help='Output path for ROM files to be downloaded', required=True)
-#Add optional arguments
+requiredargs.add_argument('-i', dest='inp', metavar='nointro.dat', help='Input DAT-file containing wanted ROMs',
+                          required=True)
+requiredargs.add_argument('-o', dest='out', metavar='/data/roms', help='Output path for ROM files to be downloaded',
+                          required=True)
+# Add optional arguments
 optionalargs = parser.add_argument_group('\033[96mOptional arguments\033[00m')
-optionalargs.add_argument('-c', dest='catalog', action='store_true', help='Choose catalog manually, even if automatically found')
-optionalargs.add_argument('-s', dest='system', action='store_true', help='Choose system collection manually, even if automatically found')
-optionalargs.add_argument('-l', dest='list', action='store_true', help='List only ROMs that are not found in server (if any)')
+optionalargs.add_argument('-c', dest='catalog', action='store_true',
+                          help='Choose catalog manually, even if automatically found')
+optionalargs.add_argument('-s', dest='system', action='store_true',
+                          help='Choose system collection manually, even if automatically found')
+optionalargs.add_argument('-l', dest='list', action='store_true',
+                          help='List only ROMs that are not found in server (if any)')
 optionalargs.add_argument('-h', '--help', dest='help', action='help', help='Show this help message')
 args = parser.parse_args()
 
-#Init variables
+# Init variables
 catalog = None
 collection = None
-wantedroms = []
-wantedfiles = []
-missingroms = []
-collectiondir = []
-availableroms = {}
-foundcollections = []
+wanted_roms = []
+wanted_files = []
+missing_roms = []
+collection_dir = []
+available_roms = {}
+found_collections = []
 
-#Validate arguments
+# Validate arguments
 if not os.path.isfile(args.inp):
     logger('Invalid input DAT-file!', 'red')
     exit()
@@ -113,187 +125,198 @@ if platform.system() == 'Linux' and args.out[-1] == '/':
 elif platform.system() == 'Windows' and args.out[-1] == '\\':
     args.out = args.out[:-1]
 
-#Open input DAT-file
+# Open input DAT-file
 logger('Opening input DAT-file...', 'green')
-datxml = ET.parse(args.inp)
-datroot = datxml.getroot()
+dat_xml = ET.parse(args.inp)
+dat_root = dat_xml.getroot()
 
-#Loop through ROMs in input DAT-file
-for datchild in datroot:
-    #Print out system information
-    if datchild.tag == 'header':
-        system = datchild.find('name').text
-        for fix in DATPOSTFIXES:
+# Loop through ROMs in input DAT-file
+for dat_child in dat_root:
+    # Print out system information
+    if dat_child.tag == 'header':
+        system = dat_child.find('name').text
+        for fix in DAT_NAME_FIXES:
             system = system.replace(fix, '')
-        catalogurl = datchild.find('url').text
-        if catalogurl in CATALOGURLS:
-            catalog = CATALOGURLS[catalogurl]
+        catalog_url = dat_child.find('url').text
+        if catalog_url in CATALOG_URLS:
+            catalog = CATALOG_URLS[catalog_url]
             logger(f'Processing {catalog}: {system}...', 'green')
         else:
             logger(f'Processing {system}...', 'green')
-    #Add found ROMs to wanted list
-    elif datchild.tag == 'game':
-        filename = datchild.attrib['name']
+    # Add found ROMs to wanted list
+    elif dat_child.tag == 'game':
+        filename = dat_child.attrib['name']
         filename = re.sub(r'\.[(a-zA-Z0-9)]{1,3}\Z', '', filename)
-        if filename not in wantedroms:
-            wantedroms.append(filename)
+        if filename not in wanted_roms:
+            wanted_roms.append(filename)
 
-#Get HTTP base and select wanted catalog
-catalogurl = None
-resp = requests.get(MYRIENTHTTPADDR, headers=REQHEADERS).text
+# Get HTTP base and select wanted catalog
+catalog_url = None
+resp = requests.get(MYRIENT_URL, headers=REQ_HEADERS).text
 resp = BeautifulSoup(resp, 'html.parser')
-maindir = resp.find('table', id='list').tbody.find_all('tr')
-for dir in maindir[1:]:
-    cell = dir.find('td')
+main_dir = resp.find('table', id='list').tbody.find_all('tr')
+for current_dir in main_dir[1:]:
+    cell = current_dir.find('td')
     if catalog in cell.a['title']:
-        catalogurl = cell.a['href']
+        catalog_url = cell.a['href']
 
-if not catalogurl or args.catalog:
+if not catalog_url or args.catalog:
     logger('Catalog for DAT not automatically found, please select from the following:', 'yellow')
-    dirnbr = 1
-    catalogtemp = {}
-    for dir in maindir[1:]:
-        cell = dir.find('td')
-        logger(f'{str(dirnbr).ljust(2)}: {cell.a["title"]}', 'yellow')
-        catalogtemp[dirnbr] = {'name': cell.a['title'], 'url': cell.a['href']}
-        dirnbr += 1
+    dir_nbr = 1
+    catalog_temp = {}
+    for current_dir in main_dir[1:]:
+        cell = current_dir.find('td')
+        logger(f'{str(dir_nbr).ljust(2)}: {cell.a["title"]}', 'yellow')
+        catalog_temp[dir_nbr] = {'name': cell.a['title'], 'url': cell.a['href']}
+        dir_nbr += 1
     while True:
         sel = inputter('Input selected catalog number: ', 'cyan')
         try:
             sel = int(sel)
-            if sel > 0 and sel < dirnbr:
-                catalog = catalogtemp[sel]['name']
-                catalogurl = catalogtemp[sel]['url']
+            if sel > 0 and sel < dir_nbr:
+                catalog = catalog_temp[sel]['name']
+                catalog_url = catalog_temp[sel]['url']
                 break
             else:
                 logger('Input number out of range!', 'red')
         except:
             logger('Invalid number!', 'red')
 
-#Get catalog directory and select wanted collection
-collectionurl = None
-resp = requests.get(f'{MYRIENTHTTPADDR}{catalogurl}', headers=REQHEADERS).text
+# Get catalog directory and select wanted collection
+collection_url = None
+resp = requests.get(f'{MYRIENT_URL}{catalog_url}', headers=REQ_HEADERS).text
 resp = BeautifulSoup(resp, 'html.parser')
-contentdir = resp.find('table', id='list').tbody.find_all('tr')
-for dir in contentdir[1:]:
-    cell = dir.find('td')
+content_dir = resp.find('table', id='list').tbody.find_all('tr')
+for current_dir in content_dir[1:]:
+    cell = current_dir.find('td')
     if cell.a['title'].startswith(system):
-        foundcollections.append({'name': cell.a['title'], 'url': cell.a['href']})
-if len(foundcollections) == 1:
-    collection = foundcollections[0]['name']
-    collectionurl = foundcollections[0]['url']
+        found_collections.append({'name': cell.a['title'], 'url': cell.a['href']})
+if len(found_collections) == 1:
+    collection = found_collections[0]['name']
+    collection_url = found_collections[0]['url']
 if not collection or args.system:
     logger('Collection for DAT not automatically found, please select from the following:', 'yellow')
-    dirnbr = 1
-    if len(foundcollections) > 1 and not args.system:
-        for foundcollection in foundcollections:
-            logger(f'{str(dirnbr).ljust(2)}: {foundcollection["name"]}', 'yellow')
-            dirnbr += 1
+    dir_nbr = 1
+    if len(found_collections) > 1 and not args.system:
+        for found_collection in found_collections:
+            logger(f'{str(dir_nbr).ljust(2)}: {found_collection["name"]}', 'yellow')
+            dir_nbr += 1
     else:
-        collectiontemp = {}
-        for dir in contentdir[1:]:
-            cell = dir.find('td')
-            logger(f'{str(dirnbr).ljust(2)}: {cell.a["title"]}', 'yellow')
-            collectiontemp[dirnbr] = {'name': cell.a['title'], 'url': cell.a['href']}
-            dirnbr += 1
+        collection_temp = {}
+        for current_dir in content_dir[1:]:
+            cell = current_dir.find('td')
+            logger(f'{str(dir_nbr).ljust(2)}: {cell.a["title"]}', 'yellow')
+            collection_temp[dir_nbr] = {'name': cell.a['title'], 'url': cell.a['href']}
+            dir_nbr += 1
     while True:
         sel = inputter('Input selected collection number: ', 'cyan')
         try:
             sel = int(sel)
-            if sel > 0 and sel < dirnbr:
-                if len(foundcollections) > 1 and not args.system:
-                    collection = foundcollections[sel-1]['name']
-                    collectionurl = foundcollections[sel-1]['url']
+            if 0 < sel < dir_nbr:
+                if len(found_collections) > 1 and not args.system:
+                    collection = found_collections[sel - 1]['name']
+                    collection_url = found_collections[sel - 1]['url']
                 else:
-                    collection = collectiontemp[sel]['name']
-                    collectionurl = collectiontemp[sel]['url']
+                    collection = collection_temp[sel]['name']
+                    collection_url = collection_temp[sel]['url']
                 break
             else:
                 logger('Input number out of range!', 'red')
         except:
             logger('Invalid number!', 'red')
-    
-#Get collection directory contents and list contents to available ROMs
-resp = requests.get(f'{MYRIENTHTTPADDR}{catalogurl}{collectionurl}', headers=REQHEADERS).text
+
+# Get collection directory contents and list contents to available ROMs
+resp = requests.get(f'{MYRIENT_URL}{catalog_url}{collection_url}', headers=REQ_HEADERS).text
 resp = BeautifulSoup(resp, 'html.parser')
-collectiondir = resp.find('table', id='list').tbody.find_all('tr')
-for rom in collectiondir[1:]:
+collection_dir = resp.find('table', id='list').tbody.find_all('tr')
+for rom in collection_dir[1:]:
     cell = rom.find('a')
     filename = cell['title']
-    romname = re.sub(r'\.[(a-zA-Z0-9)]{1,3}\Z', '', filename)
-    url = f'{MYRIENTHTTPADDR}{catalogurl}{collectionurl}{cell["href"]}'
-    availableroms[romname] = {'name': romname, 'file': filename, 'url': url}
+    rom_name = re.sub(r'\.[(a-zA-Z0-9)]{1,3}\Z', '', filename)
+    url = f'{MYRIENT_URL}{catalog_url}{collection_url}{cell["href"]}'
+    available_roms[rom_name] = {'name': rom_name, 'file': filename, 'url': url}
 
-#Compare wanted ROMs and contents of the collection, parsing out only wanted files
-for wantedrom in wantedroms:
-    if wantedrom in availableroms:
-        wantedfiles.append(availableroms[wantedrom])
+# Compare wanted ROMs and contents of the collection, parsing out only wanted files
+for wanted_rom in wanted_roms:
+    if wanted_rom in available_roms:
+        wanted_files.append(available_roms[wanted_rom])
     else:
-        missingroms.append(wantedrom)
+        missing_roms.append(wanted_rom)
 
-#Print out information about wanted/found/missing ROMs
-logger(f'Amount of wanted ROMs in DAT-file   : {len(wantedroms)}', 'green')
-logger(f'Amount of found ROMs at server      : {len(wantedfiles)}', 'green')
-if missingroms:
-    logger(f'Amount of missing ROMs at server    : {len(missingroms)}', 'yellow')
+# Print out information about wanted/found/missing ROMs
+logger(f'Amount of wanted ROMs in DAT-file   : {len(wanted_roms)}', 'green')
+logger(f'Amount of found ROMs at server      : {len(wanted_files)}', 'green')
+if missing_roms:
+    logger(f'Amount of missing ROMs at server    : {len(missing_roms)}', 'yellow')
 
-#Download wanted files
+# Download wanted files
 if not args.list:
-    dlcounter = 0
-    for wantedfile in wantedfiles:
-        dlcounter += 1
-        resumedl = False
-        proceeddl = True
-        
+    dl_counter = 0
+    for wanted_file in wanted_files:
+        dl_counter += 1
+        resume_dl = False
+        proceed_dl = True
+
         if platform.system() == 'Linux':
-            localpath = f'{args.out}/{wantedfile["file"]}'
+            local_path = f'{args.out}/{wanted_file["file"]}'
         elif platform.system() == 'Windows':
-            localpath = f'{args.out}\{wantedfile["file"]}'
-        
-        resp = requests.get(wantedfile['url'], headers=REQHEADERS, stream=True)
-        remotefilesize = int(resp.headers.get('content-length'))
-        
-        if os.path.isfile(localpath):
-            localfilesize = int(os.path.getsize(localpath))
-            if localfilesize != remotefilesize:
-                resumedl = True
+            local_path = f'{args.out}\{wanted_file["file"]}'
+
+        resp = requests.get(wanted_file['url'], headers=REQ_HEADERS, stream=True)
+        remote_file_size = int(resp.headers.get('content-length'))
+
+        if os.path.isfile(local_path):
+            local_file_size = int(os.path.getsize(local_path))
+            if local_file_size != remote_file_size:
+                resume_dl = True
             else:
-                proceeddl = False
-        
-        if proceeddl:
-            file = open(localpath, 'ab')
-            
-            size, unit = scale1024(remotefilesize)
-            pbar = ProgressBar(widgets=['\033[96m', Percentage(), ' | ', DataSize(), f' / {round(size, 1)} {unit}', ' ', Bar(marker='#'), ' ', ETA(), ' | ', FileTransferSpeed(), '\033[00m'], max_value=remotefilesize, redirect_stdout=True, max_error=False)
+                proceed_dl = False
+
+        if proceed_dl:
+            file = open(local_path, 'ab')
+
+            size, unit = scale1024(remote_file_size)
+            pbar = ProgressBar(widgets=['\033[96m', Percentage(), ' | ', DataSize(), f' / {round(size, 1)} {unit}', ' ',
+                                        Bar(marker='#'), ' ', ETA(), ' | ', FileTransferSpeed(), '\033[00m'],
+                               max_value=remote_file_size, redirect_stdout=True, max_error=False)
             pbar.start()
-            
-            if resumedl:
-                logger(f'Resuming    {str(dlcounter).zfill(len(str(len(wantedfiles))))}/{len(wantedfiles)}: {wantedfile["name"]}', 'cyan')
-                pbar += localfilesize
-                headers = REQHEADERS
-                headers.update({'Range': f'bytes={localfilesize}-'})
-                resp = requests.get(wantedfile['url'], headers=headers, stream=True)
-                for data in resp.iter_content(chunk_size=CHUNKSIZE):
+
+            if resume_dl:
+                logger(
+                    f'Resuming    {str(dl_counter).zfill(len(str(len(wanted_files))))}/{len(wanted_files)}: {wanted_file["name"]}',
+                    'cyan')
+                pbar += local_file_size
+                headers = REQ_HEADERS
+                headers.update({'Range': f'bytes={local_file_size}-'})
+                resp = requests.get(wanted_file['url'], headers=headers, stream=True)
+                for data in resp.iter_content(chunk_size=CHUNK_SIZE):
                     file.write(data)
                     pbar += len(data)
             else:
-                logger(f'Downloading {str(dlcounter).zfill(len(str(len(wantedfiles))))}/{len(wantedfiles)}: {wantedfile["name"]}', 'cyan')
-                for data in resp.iter_content(chunk_size=CHUNKSIZE):
+                logger(
+                    f'Downloading {str(dl_counter).zfill(len(str(len(wanted_files))))}/{len(wanted_files)}: {wanted_file["name"]}',
+                    'cyan')
+                for data in resp.iter_content(chunk_size=CHUNK_SIZE):
                     file.write(data)
                     pbar += len(data)
-            
+
             file.close()
             pbar.finish()
             print('\033[1A', end='\x1b[2K')
-            logger(f'Downloaded  {str(dlcounter).zfill(len(str(len(wantedfiles))))}/{len(wantedfiles)}: {wantedfile["name"]}', 'green', True)
+            logger(
+                f'Downloaded  {str(dl_counter).zfill(len(str(len(wanted_files))))}/{len(wanted_files)}: {wanted_file["name"]}',
+                'green', True)
         else:
-            logger(f'Already DLd {str(dlcounter).zfill(len(str(len(wantedfiles))))}/{len(wantedfiles)}: {wantedfile["name"]}', 'green')
+            logger(
+                f'Already DLd {str(dl_counter).zfill(len(str(len(wanted_files))))}/{len(wanted_files)}: {wanted_file["name"]}',
+                'green')
     logger('Downloading complete!', 'green', False)
 
-#Output missing ROMs, if any
-if missingroms:
-    logger(f'Following {len(missingroms)} ROMs in DAT not automatically found from server, grab these manually:', 'red')
-    for missingrom in missingroms:
-        logger(missingrom, 'yellow')
+# Output missing ROMs, if any
+if missing_roms:
+    logger(f'Following {len(missing_roms)} ROMs in DAT not automatically found from server, grab these manually:',
+           'red')
+    for missing_rom in missing_roms:
+        logger(missing_rom, 'yellow')
 else:
     logger('All ROMs in DAT found from server!', 'green')
